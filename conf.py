ed305a0619b837c93fdd94319aa90699616578e6
Dominik Stańczak <stanczakdominik@gmail.com>
2020-07-20 16:00:04 +0200

    Test pretty_urls change

--- conf.py
+++ conf.py
@@ -1049,7 +1049,7 @@ STRIP_INDEXES = True
 # This can be disabled on a per-page/post basis by adding
 #    .. pretty_url: False
 # to the metadata.
-PRETTY_URLS = True
+PRETTY_URLS = False
 
 # If True, publish future dated posts right away instead of scheduling them.
 # Defaults to False.
